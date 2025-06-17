7a6e6a14bd2f1c53467645fd0e91f12eef227f56
riki-kraus <maof5728@gmail.com>
2025-06-17 17:44:30 +0300

    angular-prod-url

---
+++ check-point-client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://checkpoint-fullstack.onrender.com/api', 
+    googleClientId: '95515128712-njsrh6ah7f90pvtf24rld5b74tca65ke.apps.googleusercontent.com'
+};
